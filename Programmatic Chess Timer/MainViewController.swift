b1b44b240ea16593ca3ebc7565f129a3054b6aeb
Caroline LaDouce <ladoucecaroline@gmail.com>
2021-02-27 17:15:27 -0500

    Added functions to click buttons

--- Programmatic Chess Timer/MainViewController.swift
+++ Programmatic Chess Timer/MainViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    var model = Model()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +26,17 @@ class MainViewController: UIViewController {
         let setTurnButton = UIButton(type: .system)
         setTurnButton.setTitle("Change Turns", for: .normal)
         setTurnButton.backgroundColor = .black
-        
+    
         view.addSubview(setTurnButton)
+
+        // Add button action:
+        setTurnButton.addTarget(self, action: #selector(setTurnButtonAction), for: .touchDown)
         
+        // Set button constraints:
         setTurnButton.translatesAutoresizingMaskIntoConstraints = false
         setTurnButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         setTurnButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
         
         self.view = view
         
@@ -41,6 +48,9 @@ class MainViewController: UIViewController {
         startPauseButton.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(startPauseButton)
+        
+        // Add button action:
+        startPauseButton.addTarget(self, action: #selector(startPauseButtonAction), for: .touchDown)
         
         NSLayoutConstraint.activate([
             startPauseButton.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor),
@@ -58,6 +68,9 @@ class MainViewController: UIViewController {
         resetButton.backgroundColor = .orange
         
         view.addSubview(resetButton)
+        
+        // Add button action:
+        resetButton.addTarget(self, action: #selector(resetButtonAction), for: .touchDown)
         
         NSLayoutConstraint.activate([
         resetButton.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor)
@@ -105,18 +118,37 @@ class MainViewController: UIViewController {
         
         
         
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Touch Handlers
+    
+    @objc func startPauseButtonAction(sender: UIButton!) {
+        self.model.startPauseGame()
     }
-    */
-
+    
+    @objc func setTurnButtonAction(sender: UIButton!) {
+        self.model.changeTurn()
+    }
+    
+    @objc func resetButtonAction(sender: UIButton!) {
+        self.model.resetGame()
+    }
+    
+    
+    
+    
+    
+    // End Class
 }
+    
+
+    
+    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+    
+
---
+++ Programmatic Chess Timer/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  Programmatic Chess Timer
+//
+//  Created by Caroline LaDouce on 2/26/21.
+//
+
+import Foundation
+
+class Model {
+   var turn = 0
+    
+    // Start/Pause button function:
+    public func startPauseGame() {
+        print("Start/Pause")
+    }
+    
+    
+    // Change turns button function:
+    public func changeTurn() {
+        print("Change Turns")
+    }
+    
+    
+    // Reset button function
+    public func resetGame() {
+        print("Reset Game")
+    }
+    
+    
+    // Top Timer clicked:
+    
+    
+    // Bottom Timer clicked:
+
+    
+}
